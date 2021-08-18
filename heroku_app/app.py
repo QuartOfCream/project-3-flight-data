6ac358e2df80a49e52f6877b438c4b2b1dceb033
NicoleRothfield <78327438+NicoleRothfield@users.noreply.github.com>
2021-08-18 14:30:10 -0500

    HTML and Flask App

--- heroku_app/app.py
+++ heroku_app/app.py
@@ -3,19 +3,13 @@ import json
 import pandas as pd
 import numpy as np
 import os
-import sqlite3
+import pickle
 
-from pandas.io import sql
 #comment
 #init app and class
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
-#initiate memory cache of database
-conn = sqlite3.connect('app/flightdelay.db')
-query = "SELECT * FROM delaydata dd left join carrierdata cd on dd.MKT_UNIQUE_CARRIER = cd.Code"
-df = pd.read_sql(query, conn)
-conn.close()
+model = pickle.load(open('heroku_app/model.pkl', 'rb'))
 
 # Route to render index.html template
 @app.route("/")
@@ -23,83 +17,23 @@ def home():
     # Return template and data
     return render_template("index.html")
 
-@app.route("/predict", methods=["POST"])
-def getData():
-    #create aggregate 1
-    airline_agg = df.groupby(["MKT_UNIQUE_CARRIER"]).size().reset_index()
-    airline_agg.columns = ["Airline", "Count"]
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
 
-    return(jsonify(json.loads(airline_agg.to_json(orient="records"))))
+    output = round(prediction[0], 2)
 
-@app.route("/sunburst", methods=["GET"])
-def sunburst():
-    #create aggregate 1
-    #data = pd.read_sql("SELECT * FROM delaydata", conn)
-    sunburstdata = df[["ORIGIN_STATE_NM", "ORIGIN", "MKT_UNIQUE_CARRIER", "CARRIER_DELAY", "WEATHER_DELAY", "NAS_DELAY", "SECURITY_DELAY", "LATE_AIRCRAFT_DELAY"]]
-    return(jsonify(json.loads(sunburstdata.to_json(orient="records")))) 
-
-@app.route("/delaydata1", methods=["GET"])
-def delaydata():
-    return(jsonify(json.loads(df.to_json(orient="records"))))
-
-@app.route("/averageDayOfWeekDepartureDelay", methods=["GET"])
-def DoWaverageDepartureDelay():
-    DoWgroup = df.groupby(["DAY_OF_WEEK", "MKT_UNIQUE_CARRIER"])
-    DptMean = DoWgroup["DEP_DELAY"].mean()
-    ArrMean = DoWgroup["ARR_DELAY"].mean()
-    DoWDeptDelaySummary = pd.DataFrame({"AvgDeptDelay": DptMean, "AvgArrDelay":ArrMean})
-    DoWAvgDeptDelaySummary = DoWDeptDelaySummary.reset_index()
-    return(jsonify(json.loads(DoWAvgDeptDelaySummary.to_json(orient="records"))))
-
+    return render_template('index.html', prediction_text='The predicted delay for this date is: {} minutes'.format(output))
 
 ####################################
 # ADD MORE ENDPOINTS
 ###########################################
 #approute for bar chart
-
-@app.route("/averageDepartureDelay", methods=["GET"])
-def averageDepartureDelay():
-    deptgroup = df.groupby(["ORIGIN", "Description"])
-    Mean = deptgroup["DEP_DELAY"].mean()
-    DeptDelaySummary = pd.DataFrame({"AvgDeptDelay": Mean})
-    AvgDeptDelaySummary = DeptDelaySummary.reset_index()
-    return(jsonify(json.loads(AvgDeptDelaySummary.to_json(orient="records"))))
-
-@app.route("/getAirports", methods=["GET"])
-def getAirports():
-    #create aggregate 1
-    airport_agg = df.groupby(["ORIGIN"]).size().reset_index()
-    airport_agg.columns = ["Airport", "Count"]
-    return(jsonify(json.loads(airport_agg.to_json(orient="records"))))
-
-@app.route("/delaySummary", methods=["GET"])
-def getDelaySummary():
-    deptgroup = df.groupby(["ORIGIN"])
-    CarrierDelaySum = deptgroup["CARRIER_DELAY"].sum()
-    WeatherDelaySum = deptgroup["WEATHER_DELAY"].sum()
-    NASDelaySum = deptgroup["NAS_DELAY"].sum()
-    SecurityDelaySum = deptgroup["SECURITY_DELAY"].sum()
-    LateAircraftDelaySum = deptgroup["LATE_AIRCRAFT_DELAY"].sum()
-    DeptDelayReasonSummary = pd.DataFrame({
-        "SumCarrierDelay": CarrierDelaySum,
-        "SumWeatherDelay": WeatherDelaySum,
-        "SumNASDelay": NASDelaySum,
-        "SumSecurityDelay": SecurityDelaySum,
-        "SumLateAircraftDelay": LateAircraftDelaySum
-    })
-    getDeptDelayReasonSummary = DeptDelayReasonSummary.reset_index()
-    return(jsonify(json.loads(getDeptDelayReasonSummary.to_json(orient="records"))))
-
-@app.route("/getCarrierData", methods=["GET"])
-def getCarriers():
-    conn = sqlite3.connect('DataTest/app/flightdelay.db')
-    query = "SELECT * FROM carrierdata"
-    carrierdf = pd.read_sql(query, conn)
-    conn.close()
-    return(jsonify(json.loads(carrierdf.to_json(orient="records"))))
-
-#app route for scatterplot
-
 #############################################################
 @app.after_request
 def add_header(r):
