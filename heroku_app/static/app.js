3010c6af4892ab3cdda572a18bfe5adf7296cf13
NicoleRothfield <78327438+NicoleRothfield@users.noreply.github.com>
2021-08-18 14:58:28 -0500

    javascript issues

--- heroku_app/static/app.js
+++ heroku_app/static/app.js
@@ -1,3 +1,7 @@
-document.getElementById('submitButton').onclick = function() {
-    document.getElementById('month').disabled = true;
+function optionChanged(newOrigin) {
+    // Fetch new data each time a new sample is selected
+    getPredictions(newOrigin);
   }
+
+  // get form field values console write line to make sure right data then pass to predict on flask app then flask app return json version of predictions
+  function getPredictions()
